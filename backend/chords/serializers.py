bde7e9e39959fa55df70411b96c79f20d30ac122
MarcSmg <dossagaby@gmail.com>
2026-05-25 18:07:21 +0100

    refactor: specify read-only fields in serializers for saved chords, progression and progresion chords

--- backend/chords/serializers.py
+++ backend/chords/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework import serializers
+
 from .models import SavedChord, Progression, ProgressionChord
+
 
 class SavedChordSerializer(serializers.ModelSerializer):
     class Meta:
         model = SavedChord
-        fields = ["id", "user", "symbol", "date_saved"]
+        fields = ["id", "user", "symbol", "voicing", "date_saved"]
+        read_only_fields = ["id", "user", "date_saved"]
+
 
 class ProgressionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Progression
+        fields = ["id", "user", "title", "created_at", "updated_at"]
+        read_only_fields = ["id", "user", "created_at", "updated_at"]
 
-    model = Progression
-    fields = ["id", "user", "title", "created_at", "updated_at"]
 
 class ProgressionChordSerializer(serializers.ModelSerializer):
-
-    model = ProgressionChord
-    fields = ["id", "progression", "order", "symbol"]
+    class Meta:
+        model = ProgressionChord
+        fields = ["id", "progression", "order", "symbol", "voicing"]
+        read_only_fields = ["id"]
